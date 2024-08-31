bbb70346fef41dd85ed2a4be33c98f18ffa9313d
padawan95 <zeli.armel@gmail.com>
2024-08-31 13:26:13 +0200

    Fix the body undefined

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -2,7 +2,7 @@ const request = require('request')
 
 const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiYXJtZWwtemVsaSIsImEiOiJjazJlamN3MXQwMDR6M2lxa2Qwc3R5Z2MwIn0.VmGq4Mv06CLelZTu3B2Veg&language=fr&limit=1'
-    request({url, json: true}, (error, {body}) => {
+    request({url, json: true}, (error, {body} = {}) => {
         if (error) {
             callback("Impossible de se connecter à l'api mapbox !", undefined)
         } else if (body.message) {
